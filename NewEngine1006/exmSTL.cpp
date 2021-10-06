015ef3221576b81ebd0f43d50dd01596ba0e013e
Red74Moon <fedoormaster@gmail.com>
2021-10-06 17:15:46 +0900

    코드 정리

--- NewEngine1006/exmSTL.cpp
+++ NewEngine1006/exmSTL.cpp
@@ -9,13 +9,15 @@ using namespace std;
 
 int main()
 {
-	// 문자열 자르기
+	// 문자열 자르기, 대문자로 변경하기 //euc-kr(한글작업잘되게), utf-8(모든언어가 작업잘되게)-다국어지원
 
 	string HelloWorld = "Hello World";
 	
 	int size = HelloWorld.size();
 	int i;
-	
+	string Capital = "";
+	string LowerCase = "";
+
 	for (i = 0; i < size; ++i)
 	{
 		if (HelloWorld[i] == ' ')
@@ -26,7 +28,14 @@ int main()
 
 	for (auto S : HelloWorld)
 	{
+		Capital.push_back(toupper(S));
 		cout << static_cast<char>(toupper(S));
+	}
+	cout << endl;
+	for (auto S : HelloWorld)
+	{
+		LowerCase.push_back(tolower(S));
+		cout << static_cast<char>(tolower(S));
 	}
 
 	cout << endl;
