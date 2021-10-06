1042db129d4a4cd618b7271ce3d0060b531acd18
Red74Moon <fedoormaster@gmail.com>
2021-10-06 14:59:58 +0900

    오버로딩

--- NewEngine1006/exmVecter.cpp
+++ NewEngine1006/exmVecter.cpp
@@ -3,20 +3,48 @@
 
 using namespace std;
 
-template<typename T>
+template<typename T, typename T1, typename T2>
 
-T Add(T a, T b)
+T2 Add(T a, T1 b)
+{
+	return static_cast<T>(a + b);
+}
+
+// template class -- 형태가 바뀌는 클래스
+template<typename T>  // vector가 이러한 형태이다.
+class NumberStore
+{
+public:
+	T Store[100];
+};
+
+//함수 오버로딩
+int Add(int a, int b)
+{
+	return a + b;
+}
+
+float Add(float a, float b)
 {
 	return a + b;
 }
 
 int main()
 {
+	NumberStore<int> IntStore;
+	NumberStore<float> FloatStore;
+
+	FloatStore.Store[0] = 10.0f;
+	IntStore.Store[0] = 10;
+
 	int c = 2;
 	int d = 3;
 
-	cout << Add<int>(c, d) << endl;
-	cout << Add<float>(3.0f, 4.4f) << endl;
+	cout << Add<int, int, int>(c, d) << endl;
+	cout << Add<int, float, float>(2, 3.0f) << endl;
+
+	cout << FloatStore.Store[0] << endl;
+	cout << IntStore.Store[0] << endl;
 
 	return 0;
 }
