c72b279e6775cbf256328d74de18c9113abd480e
Red74Moon <fedoormaster@gmail.com>
2021-10-13 09:49:39 +0900

    파일 예외처리 추가

--- 20211012/main.cpp
+++ 20211012/main.cpp
@@ -12,21 +12,27 @@
 #include "Goal.h"
 
 using namespace std;
-using std::cout; using std::cerr;
-using std::endl; using std::string;
-using std::ifstream; using std::vector;
 
 int main()
 {
-	ifstream fin; 
-	fin.open("map01.txt"); 
-	string line; 
-	while (!fin.eof()) 
-	{ 
-		getline(fin, line); 
-		cout << line << endl; 
-	} 
-	fin.close();
+    string filename("map01.txt");
+    vector<string> lines;
+    string line;
 
+    ifstream input_file(filename);
+    if (!input_file.is_open()) {
+        cerr << "Could not open the file - '"
+            << filename << "'" << endl;
+        return EXIT_FAILURE;
+    }
 
+    while (getline(input_file, line)) {
+        lines.push_back(line);
+    }
+
+    for (const auto& i : lines)
+        cout << i << endl;
+
+    input_file.close();
+    return EXIT_SUCCESS;
 }
