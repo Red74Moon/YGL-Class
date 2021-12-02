ff55a24dfd994352dc51d432c2e948f2219ec1f3
Red74Moon <fedoormaster@gmail.com>
2021-12-02 16:35:35 +0900

    서버 받고 클라리언트가 파일 전달

--- FileClient/main.cpp
+++ FileClient/main.cpp
@@ -1,11 +1,8 @@
-/*
-	Client Program(TCP)
-*/
+#define  _CRT_SECURE_NO_WARNINGS
+#define  _WINSOCK_DEPRECATED_NO_WARNINGS
 
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <WinSock2.h> // Windows
+#include <stdio.h>
+#include <WinSock2.h>
 #include <iostream>
 
 #pragma comment(lib, "ws2_32.lib")
@@ -14,72 +11,55 @@ using namespace std;
 
 int main()
 {
-	WSAData	wsaData;
+	FILE* destinationFile = nullptr;
+	SOCKET ServerSocket;
+	SOCKADDR_IN ServerAddr;
+	WSAData wsaData;
 
-	// server socket
-	SOCKET hServerSocket;
-
-	// ip주소를 넣기 위한 구조체
-	SOCKADDR_IN serverAddr;
-
-	// 1.WindSock 초기화 (윈도우용), 리눅스는 없다.
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	int Result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (Result != 0)
 	{
-		cout << "Error WSASartup" << endl;
+		printf("WSAStartup");
 		exit(-1);
 	}
 
-	// 2.Create Socket
-	hServerSocket = socket(PF_INET, SOCK_STREAM, 0);
-	if (hServerSocket == INVALID_SOCKET)
+	ServerSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (ServerSocket == INVALID_SOCKET)
 	{
-		cout << "Error socket" << endl;
+		printf("socket");
 		exit(-1);
 	}
 
-	// ip주소 구조체의 사이즈 만큼 초기화(0으로..)
-	memset(&serverAddr, 0, sizeof(serverAddr));
+	memset(&ServerAddr, 0, sizeof(SOCKADDR_IN));
+	ServerAddr.sin_family = PF_INET; //IP V4
+	ServerAddr.sin_port = htons(12345); // 서버 포트
+	ServerAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); //서버 아이피
 
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // ip 넣는 것, 목적지 주소
-	serverAddr.sin_port = htons(9190); // port
+	connect(ServerSocket, (SOCKADDR*)&ServerAddr, sizeof(ServerAddr));
 
-	// 3. connect
+	destinationFile = fopen("Tree.png", "rb");
 
-	if (connect(hServerSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	char Buffer[1024] = { 0, };
+	int ReadLength = 0;
+
+	while ((ReadLength = fread(Buffer, sizeof(char), sizeof(Buffer), destinationFile)) != 0)
 	{
-		cout << "Error connect" << endl;
-		exit(-1);
+		int SendLength = send(ServerSocket, Buffer, ReadLength, 0);
+		cout << "Lenght => " << SendLength << endl;
 	}
-/*  -------------------------------------------------------------------------  */
-	FILE* dst;
 
-	char buf[1024] = "";
-	int len = 0;
+	shutdown(ServerSocket, SD_SEND);
 
-	dst = fopen("Tree_copy.png", "wb");
+	//서버에서 보내는 연결이 끊기면 완료  정보를 보내줌.
+	char Message[] = "End Of file.";
+	send(ServerSocket, Message, sizeof(Message), 0);
 
-	while (1)
-	{
-		memset(buf, 0, sizeof(buf));
-		recv(hServerSocket, buf, sizeof(buf), 0);
-			
-		len = fwrite(buf, sizeof(char), sizeof(buf), dst);
-		fflush(dst);
-		cout << "len =》 " << len << endl;
-		if (len == 0)
-		{
-			break;
-		}
-	}
-	cout << "Copy OK!" << endl;
-	fclose(dst);
-	dst = nullptr;
-/*  -------------------------------------------------------------------------  */
-	// 마무리
-	closesocket(hServerSocket); // 내쪽 서버 정보 지우고...
+	fclose(destinationFile);
+	closesocket(ServerSocket);
 
 	WSACleanup();
 
 	return 0;
 }
+
+
--- FileServer/main.cpp
+++ FileServer/main.cpp
@@ -1,10 +1,8 @@
-/*
-	Server Program(TCP)
-*/
-#define _CRT_SECURE_NO_WARNINGS
-#include <WinSock2.h> // Windows
-#include <iostream>
+#define  _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
+#include <WinSock2.h>
+#include <iostream>
 
 #pragma comment(lib, "ws2_32.lib")
 
@@ -12,115 +10,69 @@ using namespace std;
 
 int main()
 {
-	WSAData	wsaData;
+	FILE* sourceFile = nullptr;
 
-	// 외부에서 들어올 수 있도록 서버에 구멍을 만들어 준다.
-	SOCKET hServerSocket; //h - 핸들(handle)
+	SOCKET ServerSocket;
+	SOCKET ClientSocket;
 
-	// client socket, 클라이언트 접속자 수 만큼 필요
-	SOCKET hClientSocket;
+	SOCKADDR_IN ServerAddr;
+	SOCKADDR_IN ClientAddr;
 
-	// ip주소를 넣기 위한 구조체
-	SOCKADDR_IN serverAddr;
-	// client ip addr
-	SOCKADDR_IN clientAddr;
+	WSAData wsaData;
 
-	// 1.WindSock 초기화 (윈도우용), 리눅스는 없다.
-	// 윈도우 소켓 라이브러리를 사용하겠다. 호출
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	int Result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (Result != 0)
 	{
-		cout << "Error WSASartup" << endl;
+		printf("WSAStartup");
 		exit(-1);
 	}
 
-	// 2.Create Socket 구멍을 뚫을 파이프를 만든다.
-	// PF_INET을 사용한다.(Protocol Famliy 프로토콜 패밀리 약자)
-	// 연결지향으로 사용한다.(SOCK_STREAM와 SOCK_DGRAM-(보내는 중심) 두가지가있다.)
-	// 0 보통 0을 사용
-	// SOCK_STREAM : TCP로 만들어서 연결지향, 순서까지 보장해서 보내어 준다.
-	// SOCK_DREAM : 택배 보내는 것과 같다. 순서보장이 없다. 
-	//              중간 손실이 생길 수 있다. 앞 뒤가 바뀔 수 있다. (=UDP)
-	hServerSocket = socket(PF_INET, SOCK_STREAM, 0);
-	if (hServerSocket == INVALID_SOCKET) // INVALID_SOCKET ~0 
+	ServerSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (ServerSocket == INVALID_SOCKET)
 	{
-		// 예외처리 부분
-		cout << "Error socket" << endl;
+		printf("socket");
 		exit(-1);
 	}
 
-	// ip주소 구조체의 사이즈 만큼 초기화(0으로..) 사이즈 잡고 한방에 보내어 준다.
-	memset(&serverAddr, 0, sizeof(serverAddr));
+	memset(&ServerAddr, 0, sizeof(SOCKADDR_IN));
+	ServerAddr.sin_family = PF_INET; //IP V4
+	ServerAddr.sin_port = htons(12345);
+	ServerAddr.sin_addr.s_addr = INADDR_ANY; //아무거나
 
-	serverAddr.sin_family = AF_INET; // TCP_IP 사용(IPv4)
-	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY); // 4바이트 IP만들어 준다. ip 넣는 곳
-	serverAddr.sin_port = htons(9190); // port  (0~65535) 컴퓨터의 프로그램별 접속 번호
+	bind(ServerSocket, (SOCKADDR*)&ServerAddr, sizeof(ServerAddr));
 
-	// 3.ip, port binding
-	//         사용하는 소켓            주소             사이즈
-	// 소켓(포트) 충돌, 이미사용중, 포트는 기본적으로 동시에 사용할 수 없다. 
-	if (bind(hServerSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-	{
-		cout << "Error bind" << endl;
-		exit(-1);
-	}
-
-	// 4 Linten (대기) (0 시간..)
-	if (listen(hServerSocket, 0) == SOCKET_ERROR)
-	{
-		cout << "Error Linten" << endl;
-		exit(-1);
-	}
-	cout << "Ok Linten" << endl;
-	// 5.accept - client의 접속 실제 접속시 하나를 더 만들어서 클라이언트 소켓을 만든다.
+	listen(ServerSocket, 5);
 
 	while (1)
 	{
+		int ClientAddrLength = sizeof(ClientAddr);
+		ClientSocket = accept(ServerSocket, (SOCKADDR*)&ClientAddr, &ClientAddrLength);
 
-		int clientAddrSize = sizeof(clientAddr);
+		sourceFile = fopen("Tree_Copy.png", "wb");
 
-
-		hClientSocket = accept(hServerSocket, (SOCKADDR*)&clientAddr, &clientAddrSize); // 연결 정보를 준다. 
-		if (hClientSocket == SOCKET_ERROR)
+		char Buffer[1024] = { 0, };
+		int RecvLength = 0;
+		while ((RecvLength = recv(ClientSocket, Buffer, sizeof(Buffer), 0)) != 0)
 		{
-			cout << "Error accept" << endl;
-			exit(-1);
+			fwrite(Buffer, sizeof(char), RecvLength, sourceFile);
+			cout << "RecvLength => " << RecvLength << endl;
+			memset(Buffer, 0, sizeof(Buffer));
 		}
 
-		cout << "Ok accept" << endl;
-/*  -------------------------------------------------------------------------  */
+		recv(ClientSocket, Buffer, sizeof(Buffer), 0);
 
-		//파일의 위치를 알려주는 구조체
-		FILE* src;
+		printf("from client : %s\n", Buffer);
 
-		char buf[1024] = "";
-		int len = 0;
+		fclose(sourceFile);
 
-		src = fopen("Tree.png", "rb");
-
-		cout << "Ok file open" << endl;
-
-		while (1)
-		{
-			memset(buf, 0, sizeof(src));
-			len = fread(buf, sizeof(char), sizeof(buf), src);
-			cout << "len =》 " << len << endl;
-			if (len == 0)
-			{
-				break;
-			}
-			send(hClientSocket, buf, sizeof(buf), 0);
-		}
-		fclose(src);
-		src = nullptr;
-/*  -------------------------------------------------------------------------  */
+		closesocket(ClientSocket);
+		printf("disconnect\n");
 	}
 
-	// 7.마무리
-	closesocket(hClientSocket);
-	closesocket(hServerSocket);
+	closesocket(ServerSocket);
 
-	// 역시 윈도우에서만 사용, 소켓청소
 	WSACleanup();
 
 	return 0;
 }
+
