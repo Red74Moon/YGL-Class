4fd227cd1c75e1b5077e89a1e85768af5b158267
Red74Moon <fedoormaster@gmail.com>
2021-12-17 16:00:11 +0900

    login

--- MySQLExample/main.cpp
+++ MySQLExample/main.cpp
@@ -7,44 +7,86 @@
 #include "jdbc/cppconn/statement.h"
 #include "jdbc/cppconn/prepared_statement.h"
 
-#pragma comment(lib, "debug/mysqlcppconn.lib")
+#ifdef _DEBUG
+#pragma comment(lib, "./debug/mysqlcppconn.lib")
+#else
+#pragma comment(lib, "./mysqlcppconn.lib")
+#endif
 
 using namespace std;
 
 int main()
 {
-	sql::Driver* driver;
-	sql::Connection* connection;
-	sql::Statement* statement;
-	sql::ResultSet* resultset;
+	sql::Driver* driver = nullptr;
+	sql::Connection* connection = nullptr;
+	sql::Statement* statement = nullptr;
+	sql::ResultSet* resultset = nullptr;
+	sql::PreparedStatement* preparedstatement = nullptr;
 
-	//Workbanch 켜기
-	driver = get_driver_instance(); // 엔진 생성
-	
-	//연결 버튼 누르기
-	connection = driver->connect("tcp://127.0.0.1:3306", "root", "1234");
-	if (connection == nullptr)
+	string userid;
+	string userpassword;
+	bool isLogin = false;
+
+	while (!isLogin)
 	{
-		cout << "connection error!" << endl;
-		exit(-1);
+		cout << "유저아이디 : ";
+		cin >> userid;
+		cout << "유저비밀번호 : ";
+		cin >> userpassword;
+
+
+		//workbench 켜기
+		driver = get_driver_instance();
+		//연결 버튼 누르기
+		connection = driver->connect("tcp://127.0.0.1:3306", "root", "1234");
+		if (connection == nullptr)
+		{
+			cout << "connection error." << endl;
+			exit(-1);
+		}
+
+		//use ygl;
+		connection->setSchema("ygl");
+		//Query typing
+		statement = connection->createStatement();
+		//쿼리실행
+		//resultset = statement->executeQuery("SELECT * from film_actor limit 10");
+
+		//로그인 
+		preparedstatement = connection->prepareStatement("SELECT * from member where user_id = ? and user_password = ?");
+		preparedstatement->setString(1, userid);
+		preparedstatement->setString(2, userpassword);
+		preparedstatement->execute();
+		resultset = preparedstatement->getResultSet();
+
+		if (resultset && resultset->rowsCount() > 0)
+		{
+			cout << "로그인 성공" << endl;
+			isLogin = true;
+		}
+		else
+		{
+			cout << "로그인 실패" << endl;
+		}
 	}
-
-	//use Database
-	connection->setSchema("ygl");
-
-	//Quarry
-	statement = connection->createStatement();
-	resultset = statement->executeQuery("SELECT 'Hello Word' AS message");
 
 	//Display
-	while (resultset->next())
-	{
-		cout << resultset->getString("message") << endl;
-	}
+	//for (;resultset->next();)
+	//{
+	//	cout << resultset->getString("film_id") << endl;
+	//}
+
+	//while (resultset->next())
+	//{
+	//	cout << resultset->getString("film_id") << endl;
+	//	cout << resultset->getString("actor_id") << endl;
+	//	cout << resultset->getString("last_update") << endl;
+	//}
 
 	delete resultset;
 	delete statement;
 	delete connection;
+
 
 	return 0;
 }
